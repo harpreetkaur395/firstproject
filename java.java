211a30ebbb61aa3b1b43e5206a1688a8aa943072
harpreetkaur395 <52628240+harpreetkaur395@users.noreply.github.com>
2019-07-07 10:05:44 -0400

    Update java.java

--- java.java
+++ java.java
@@ -1,7 +1,10 @@
 public classMain
 {
-public static void main(String[]args{
-
+public static void main (String[]args{
+  int x=5;
+system.out.println("Hello worl");
+system.out.println("java programming");
+    
 
 
 
